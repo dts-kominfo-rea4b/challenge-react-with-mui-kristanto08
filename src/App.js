24cd4ab5aaffb647e7b927341305d42f31ebd5cf
kristanto08 <113617653+kristanto08@users.noreply.github.com>
2022-10-03 21:21:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import Contact from './components/Contact';
+import ContactForm from './components/ContactForm';
+import {Card} from '@mui/material';
 
 // Uncomment untuk memuat daftar kontak
-// import contactsJSON from './data/contacts.json';
+import contactsJSON from './data/contacts.json';
 const App = () => {
   // Masukkan Header dan lakukan map untuk Contact ke dalam div App
   // untuk membuat daftar kontak bisa menggunakan MUI list
@@ -9,9 +14,29 @@ const App = () => {
 
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
+  const [contactOn, setContactOn] = useState(
+    contactsJSON
+  );
 
+  const onAddContactHandler= (contact)=>{
+    setContactOn([...contactOn,contact])
+  }
+ 
+  
   return (
     <div className="App">
+      <Header/>
+      <Card variant="outlined" className='cardMenu'>
+        <div className="contactFormNew">
+          <ContactForm  className="contactFormNew" addContact={onAddContactHandler}/>
+        </div>
+        <div className='contactNew'>
+          {contactOn.map((contact, i) => {
+            return <Contact key={i} data={contact} />;
+          })}
+        </div>
+        
+      </Card>
     </div>
   );
 };
