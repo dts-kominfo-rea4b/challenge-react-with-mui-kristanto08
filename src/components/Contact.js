c8ef0bd4b2aa3434db494e1fbfad73556f564891
kristanto08 <113617653+kristanto08@users.noreply.github.com>
2022-10-03 21:15:37 +0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,25 @@
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
 import React from 'react';
+import './Contact.css';
+
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
-    return (<></>);
+    return (
+    <>
+    <div className='card'>
+        <img src={data.photo} alt="foto" className="image"/>
+        <div className='contactInfo'>
+            <h1>{data.name}</h1>
+            <h4 className='contactRev'>{data.phone}</h4>
+            <h4 className='contactRev'>{data.email}</h4>
+        </div>
+    </div>
+    </>
+    );
 };
 
 export default Contact;
