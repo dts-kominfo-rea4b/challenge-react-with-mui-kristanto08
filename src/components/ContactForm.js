98df382b688f3d2a63b100df6053bdaf8dbeda8f
kristanto08 <113617653+kristanto08@users.noreply.github.com>
2022-10-03 21:19:00 +0700

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,13 +2,87 @@
 // https://mui.com/material-ui/react-text-field/#basic-textfield
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
+import React, {useState} from "react";
+import { TextField,Card, Typography, Button } from "@mui/material";
+import { ConstructionRounded } from "@mui/icons-material";
 
-const ContactForm = () => {
+const ContactForm = ({addContact}) => {
     // Form berisi name, phone, email, dan photo url
     // Buatlah state newContact berupa objek sesuai dengan data yang ada
+    const [newContact, setNewContact] = useState([
+      {
+        name: "",
+        phone: "",
+        email: "",
+        photo: "",
+      },
+      
+    ]
+    )
+  const onChangeHandler=(event)=>{
+    const { name, value } = event.target;
+    setNewContact({...newContact, [name]:value}) 
+}
+ 
+
+const onSubmitHandler=(event)=>{
+  event.preventDefault();
+  addContact(newContact);
+  setNewContact({
+        name: "",
+        phone: "",
+        email: "",
+        photo: "",
+      })
+}
 
     return (
-        <> </>
+        <>
+        <Card sx={{display:'flex', flexDirection:'column', backgroundColor:'#F3F1EB', marginRight:'50px'}} >
+        <TextField 
+        required
+        id="standard-required"
+        label="name"
+        name="name"
+        variant="outlined"
+        onChange={onChangeHandler}
+        value={newContact.name}
+        sx={{marginBottom:'30px', marginTop:'20px', marginLeft:'10PX', marginRight:'10px'}}
+        />
+        <TextField 
+        required
+        id="standard-required"
+        label="phone"
+        name="phone"
+        variant="outlined"
+        onChange={onChangeHandler}
+        value={newContact.phone}
+        sx={{marginBottom:'30px',  marginLeft:'10PX', marginRight:'10px'}}
+        />
+        <TextField 
+        required
+        id="standard-required"
+        label="email"
+        name="email"
+        variant="outlined"
+        onChange={onChangeHandler}
+        value={newContact.email}
+        sx={{marginBottom:'30px',  marginLeft:'10PX', marginRight:'10px'}}
+        />
+        <TextField 
+        required
+        id="standard-required"
+        label="photo"
+        name="photo"
+        variant="outlined"
+        onChange={onChangeHandler}
+        value={newContact.photo}
+        sx={{marginBottom:'30px',  marginLeft:'10PX', marginRight:'10px'}}
+        />
+        <Button onClick={onSubmitHandler}>ADD NEW</Button>
+        </Card>
+        </>
+        
     );
 }
 
